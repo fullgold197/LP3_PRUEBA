160895c707bc9865dbe0a14372d56fae583d5af4
Oscar Vilca Rivera <oevr1997@gmail.com>
2020-07-09 14:58:37 -0500

    Se agrego

---
+++ aprobado.py
@@ -0,0 +1,10 @@
+#Ejercicio que determine si un estudiante esta aprobado o no
+#Autor: Oscar Vilca Rivera
+
+def determinaraprobado(promedio):
+    if promedio>=11:
+        resultado="Aprobado"
+    else:
+        resultado="Desaprobado"
+    return resultado
+
